d4aa082bddd7e4f9c23ee3f80d4de8a482287699
Colm Moore <colm.moore3@gmail.com>
2024-09-01 18:44:08 +1000

    Added multiply 2

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.add = add;
+exports.multiply = multiply;
 function add(a, b) {
     return a + b;
+}
+function multiply(a, b) {
+    return a * b;
 }
