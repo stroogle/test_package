a6d5fc95dc3b11ecedf32d8b583f8a27bc8be2af
Colm Moore <colm.moore3@gmail.com>
2024-09-01 19:07:59 +1000

    created module

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,2 +1,4 @@
-declare function add(a:number, b:number): number
-declare function multiply(a: number, b:number): number
+declare module test_package {
+    export function add(a:number, b:number): number
+    export function multiply(a: number, b:number): number
+}
